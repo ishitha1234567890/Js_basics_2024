62e4c01503d2d7965646a56c6385f196c52e5801
ISHITHA RAWAT <52525172+ishitha1234567890@users.noreply.github.com>
2024-09-12 05:00:04 +0000

    DatatypeAndVariables

---
+++ Basics/01_Variables.js
@@ -0,0 +1,19 @@
+const accountId=123
+let acctEmail="abc@gmail.com"
+var accountPassword="123hello"
+accountCity="Hyd"
+
+
+//accountId=321
+acctEmail="efg@gmail.com"
+accountPassword="234hello"
+accountCity="Del"
+let accountCountry;
+
+console.log(accountId);
+console.log(acctEmail);
+console.log(accountPassword);
+console.log(accountCity);
+console.table([accountId,accountCity,accountCountry
+])
+/*Prefer not to use var because of issue in block scope and function scope*/ 
---
+++ Basics/02_Datatypes.js
@@ -0,0 +1,20 @@
+"use strict" //treat all JS code as newer version
+//alert("hello")   // we are working in node so this will not work but if we do same in  console it will work as it is inside doc
+let name="a"
+let age=24
+let isFemal=true
+
+////Primitive dataypes
+
+//number=>2 to power 53
+//bigint
+//string
+//boolean=>true or false
+//null=>stand-alone value
+//undefined=>value is not yet defined
+//symbol=>used in react for uniqueness
+
+//Object
+console.log(typeof age)//number
+console.log(typeof null)//object
+console.log(typeof undefined)//undefined
