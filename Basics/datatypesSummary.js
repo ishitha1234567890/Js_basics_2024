7610cdd7bd5e37bdffe68f233341e4122faa866f
Ishitha Rawat <ishitharawat19dec@gmail.com>
2024-09-26 09:20:18 +0530

    commiting

--- Basics/datatypesSummary.js
+++ Basics/datatypesSummary.js
@@ -5,9 +5,9 @@
 // 1.Primitive(call by value)-->string,Number,Boolean,Null,Undefined,Symbol,BigInt
 
 console.log(typeof null);//type of value is object
-const id=Symbol("1233");//type of value is symbol
+const id=Symbol("1233");//type of value is Symbol
 const id2=Symbol("1233");
-console.log(id==id2);
+console.log(id==id2);//false
 const BigIntval=12334223423423423n;
 console.log(typeof BigIntval);//type of value is BigInt
 
@@ -25,3 +25,31 @@ const myFunc=function(){
 }
 myFunc();
 console.log(typeof myFunc);//func object
+
+
+
+/*Call by Value: A copy of the value is passed. Changes to the copy do not affect the original.
+Call by Reference: A reference to the original object is passed. Changes to the reference do affect the original object.
+*/
+
+let aa = 10;
+
+function modifyValue(x) {
+    x = 20;
+}
+
+modifyValue(aa);
+console.log(aa); // Output: 10 (Original value is not changed)
+
+
+
+let obj = { name: "Alice" };
+
+function modifyObject(o) {
+    o.name = "Bob";
+}
+
+modifyObject(obj);
+console.log(obj.name); // Output: Bob (Original object is changed)
+
+
