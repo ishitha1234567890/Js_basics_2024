a5eb2c555e18b25ce9b33aa3e9eaf6e67f1c40bb
Ishitha Rawat <ishitharawat19dec@gmail.com>
2024-10-03 11:03:51 +0530

    commiting

--- Basics_02/01_arrays.js
+++ Basics_02/01_arrays.js
@@ -1,4 +1,7 @@
 //arrays
 const myarr=[1,2,3,4,5];//each is called elements
+//Js arrays are resizbale and mix of diff datatype,arrays cannot be accessed by value can be acccessed by index only starting from 0
+//array copy operation only gives shallow copies
+
 
 
